022852ebd94059fba9db47c9251097d5b915a0a2
Aamsher <hpandian008@gmail.com>
2023-08-02 19:17:35 +0530

    optimized the code to a single callback

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -11,26 +11,46 @@ const initialState = {
 const Main = () => {
   const [speedObject, setSpeedObject] = useState(initialState);
 
-  const callbackKmph = (speed) => {
-    speed = parseInt(speed);
-    setSpeedObject({
-      Kmph: speed,
-      Mph: speed * 0.621371, 
-    });
-  };
+  const callBack = (Kmph, Mph) => {
+   
+    if(Kmph){
+        Kmph = parseInt(Kmph);
+        setSpeedObject({
+       Kmph: Kmph,
+       Mph: Kmph * 0.621371, 
+     });
+    } 
+    else if (Mph) {
+        Mph = parseInt(Mph);
+        setSpeedObject({
+       Kmph: Mph / 0.621371, 
+       Mph: Mph,
+     });
+    }
+  }
 
-  const callbackMph = (speed) => {
-    speed = parseInt(speed);
-    setSpeedObject({
-      Kmph: speed / 0.621371, 
-      Mph: speed,
-    });
-  };
+//   const callbackKmph = (speed) => {
+//     speed = parseInt(speed);
+//     setSpeedObject({
+//       Kmph: speed,
+//       Mph: speed * 0.621371, 
+//     });
+//   };
+
+//   const callbackMph = (speed) => {
+//     speed = parseInt(speed);
+//     setSpeedObject({
+//       Kmph: speed / 0.621371, 
+//       Mph: speed,
+//     });
+ 
 
   return (
     <div>
-      <Combined type="Kmph" speed={speedObject.Kmph} callback={callbackKmph} />
-      <Combined type="Mph" speed={speedObject.Mph} callback={callbackMph} />
+      <Combined type="Kmph" speed={speedObject.Kmph} callback={callBack} />
+      <Combined type="Mph" speed={speedObject.Mph} callback={callBack} />
+      {/* <Combined type="Kmph" speed={speedObject.Kmph} callback={callbackKmph} />
+      <Combined type="Mph" speed={speedObject.Mph} callback={callbackMph} /> */}
     </div>
   );
 };
